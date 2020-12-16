b9a2311410751960508ce12bba15b2d783fdcfac
WAN NUR ATIQAH BINTI WAN AHMAD YUSRI <atiqahyusri05@gmail.com>
2020-12-16 23:51:50 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title>E-Learning Web Analytics</title>
+    <link rel="stylesheet" type="text/css" href="css/styleheader.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <header class="header">
+    <nav class="navbar navbar-style">
+        <div class="container-fluid">
+            <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+            </button>
+
+            <a href=''><img class='logo2' src='image/elablue_1.png'></a>
+			
+            </div>
+			
+			<div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href='home.html'>Home</a></li>
+                    </ul>
+
+            </div>
+			
+			
+        </div>
+
+    </nav>
+
+</header>
+<body>
+</html>
